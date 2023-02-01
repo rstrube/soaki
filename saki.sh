eccc1184230650236971e6781f7b1159f4e072a8
Robert <rstrube@gmail.com>
2023-02-01 11:58:37 -0700

    Fixed nvidia pacman hook

--- saki.sh
+++ saki.sh
@@ -665,12 +665,15 @@ Operation=Upgrade
 Operation=Remove
 Type=Package
 Target=nvidia
+Target=linux
+# Change the linux part above and in the Exec line if a different kernel is used
 
 [Action]
-Description=Update Nvidia module in initcpio
+Description=Update NVIDIA module in initcpio
 Depends=mkinitcpio
 When=PostTransaction
-Exec=/usr/bin/mkinitcpio -P
+NeedsTargets
+Exec=/bin/sh -c 'while read -r trg; do case $trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
 EOT
 
 }
